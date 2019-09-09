710399f60a2c0a952b175295df362bfae3b856dd
Lauren McCoy <lmccoy8911@gmail.com>
2019-09-09 22:23:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
-require_relative 'config/environment'
-
+require_relative './config/environment'
+require 'pry'
 class App < Sinatra::Base
+  
+  enable :sessions 
+  
+  get '/' do 
+    erb :index 
+  end 
+
+  post '/checkout' do 
+    @session = params[:item] 
+  end 
+
 end
